7b31fb070eced826953ce79e365b7de3240413c2
AvocadoDicaprio <rainylo918@gmail.com>
2026-01-09 10:58:46 +0800

    Fine-tune button positions closer to input box

--- chatbot_app.py
+++ chatbot_app.py
@@ -47,14 +47,14 @@ st.markdown("""
 
     /* LEFT BUTTON: Clear (Inside the 1st Column) */
     [data-testid="column"]:nth-of-type(1) div.stButton > button {
-        left: calc(50% - 375px) !important; /* Positioned just left of input box */
+        left: calc(50% - 380px) !important; /* Right next to left edge of input */
     }
 
     /* RIGHT BUTTON: Search (Inside the 3rd Column) */
     [data-testid="column"]:nth-of-type(3) div.stButton > button {
         left: auto !important;
         right: auto !important;
-        left: calc(50% + 335px) !important; /* Positioned just right of input box */
+        left: calc(50% + 340px) !important; /* Right next to right edge of input */
     }
 
     /* Mobile Responsive Logic */
